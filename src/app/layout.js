cd0b7e1a1fa7b1c4650b47bf181478a8d2cdd926
Usman Naveed <salman.r.afridi@hotmail.com>
2025-08-24 17:10:20 +0100

    description added

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,8 @@ import Footer from "./components/footer/footer";
 
 export const metadata = {
   title: "Brooklyn Kabab",
-  description: "Brooklyn Kabab",
+  description:
+    "Brooklyn Kabab With a menu featuring vegetarian and vegan options, it caters to diverse palates while maintaining a homely, community-driven vibe.",
 };
 
 export default function RootLayout({ children }) {
